0c86408507b4740ec142f13d11179f99ae8f6a73
James McEwen <james@silverfoxmedia.co>
2025-05-09 15:43:34 -0500

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -20,14 +20,24 @@ router.get('/profile', requireAuth, async (req, res) => {
 // Update user profile
 router.put('/profile', requireAuth, async (req, res) => {
   try {
-    
-    const updates = (({ name, username, email, phone, address }) => ({ name, username, email, phone, address }))(req.body);
+    console.log('Updating request received:', req.body);
+
+    const updates = {
+      name: req.body.name,
+      username: req.body.username,
+      email: req.body.email,
+      phone: req.body.phone,
+      address: req.body.address
+    };
+    console.log('Updates to be applied:', updates);
 
     const updatedUser = await User.findByIdAndUpdate(req.userId, updates, {
       new: true,
       runValidators: true
     }).select('-password');
+
     if (!updatedUser) return res.status(404).json({ message: 'User not found' });
+    console.log('Updated user:', updatedUser);
     res.json(updatedUser);
   } catch (err) {
     console.error('Profile update error:', err);
@@ -36,36 +46,3 @@ router.put('/profile', requireAuth, async (req, res) => {
 });
 
 module.exports = router;
-
-
-
-//old code
-// routes/userRoutes.js
-/*const express = require('express');
-const router = express.Router();
-const User = require('../models/User');
-const requireAuth = require('../middleware/requireAuth');
-
-router.get('/profile', requireAuth, async (req, res) => {
-  try {
-    const user = await User.findById(req.userId).select('-password');
-
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    res.status(200).json({
-      name: user.name,
-      phone: user.phone,
-      email: user.email,
-      kyc_status: user.kyc_status,
-      createdAt: user.createdAt
-    });
-  } catch (error) {
-    console.error('User profile error:', error);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
-
-module.exports = router;
-*/
